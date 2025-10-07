70843bccccfc66330c245e0b9a1b5dcce852a75f
TakedaB <contatobrunotakeda@gmail.com>
2025-10-07 20:28:46 -0300

    Fix: correct SVG syntax and icon imports

--- app/components/Layout/Sidebar.jsx
+++ app/components/Layout/Sidebar.jsx
@@ -3,7 +3,7 @@ import { usePathname } from "next/navigation";
 import Link from "next/link";
 import Logo from "./Logo";
 
-import {DashboardIcon, StudentsIcon, AddStudentIcon,TeachersIcon,FinanceIcon,UserIcon,} from "app/components/Icons/Icons";
+import {DashboardIcon, StudentsIcon, AddStudentIcon,TeachersIcon,FinanceIcon,UserIcon,} from "../Icons/Icons";
 
 const menuItems = [
   { name: "Dashboard", icon: DashboardIcon, path: "/dashboard" },
@@ -31,13 +31,18 @@ export default function Sidebar() {
           return (
             <Link key={item.path} href={item.path}>
               <div
-                className={`flex items-center gap-3 p-3 rounded-lg transition ${
+                className={`group flex items-center gap-3 p-3 rounded-lg transition-all ${
                   isActive
                     ? "bg-white text-[#4F46E5] font-semibold"
                     : "hover:bg-[#635DF7]"
                 }`}
               >
-                <Icon className="w-5 h-5" />
+                <Icon 
+                className={`w-5 h-5 ${
+                  isActive ? 'text-[#4F46E5]' : 'text-white group-hover:text-white'
+                }`} 
+                />
+                
                 <span>{item.name}</span>
               </div>
             </Link>
